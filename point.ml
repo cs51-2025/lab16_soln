97b5363729e49d36be2f5e360448b5d37bcf39bb
CS51 Heads <cs51heads@gmail.com>
2025-01-27 20:26:00 +0000

    Automated distribution code deploy

---
+++ point.ml
@@ -0,0 +1,20 @@
+(*
+                             CS51 Lab 16
+                     Object-Oriented Programming
+                        Two-Dimensional Points
+
+See the file point.mli for documentation of the interface.
+ *)
+
+type point = float * float ;;
+
+let add (x1, y1) (x2, y2) =
+  x1 +. x2, y1 +. y2 ;;
+
+let scale s (x, y) =
+  s *. x, s *. y ;;
+  
+let offset (x, y) distance angle =
+  x +. distance *. cos angle,
+  y +. distance *. sin angle ;;
+
